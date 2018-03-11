0813cf6d43133b9e673f34d1c3340c9c0d8a7acc
Kartik Soni <sonikartik98@outlook.com>
2018-03-11 20:00:18 +0530

    contract class, helper class, provider class setup initiated

--- app/src/main/java/com/example/kartik/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/kartik/inventoryapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
@@ -32,5 +33,15 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_dummy_data:
+                /*addDummyData();
+                adapter.swapCursor(dbHelper.readStock());*/
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/kartik/inventoryapp/data/StockProvider.java
@@ -0,0 +1,8 @@
+package com.example.kartik.inventoryapp.data;
+
+/**
+ * Created by KARTIK on 3/11/2018.
+ */
+
+public class StockProvider {
+}
