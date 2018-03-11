c682ad7a5820df090becf513214e1457ebbd222b
Kartik Soni <sonikartik98@outlook.com>
2018-03-11 16:00:57 +0530

    float button added and required classes created

---
+++ app/src/main/java/com/example/kartik/inventoryapp/EditorsActivity.java
@@ -0,0 +1,8 @@
+package com.example.kartik.inventoryapp;
+
+/**
+ * Created by KARTIK on 3/11/2018.
+ */
+
+public class EditorsActivity {
+}
--- app/src/main/java/com/example/kartik/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/kartik/inventoryapp/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.kartik.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final FloatingActionButton fab = findViewById(R.id.fab);
+        fab.show();
     }
 }
